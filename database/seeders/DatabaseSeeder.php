adc4a48987c8f2d224f15d7581d839393df12943
Micah Mustaham <micahmustaham@gmail.com>
2025-08-11 22:22:17 +0800

    updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
             ProductCategorySeeder::class,
             ProductSubCategorySeeder::class,
             ProductSeeder::class,
-            ProductVariantSeeder::class,
-            StockMovementSeeder::class
+//            ProductVariantSeeder::class,
+//            StockMovementSeeder::class
         ]);
     }
 }
